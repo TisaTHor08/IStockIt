37905742f76e960c1584c3bc95ce04ccd3fead28
Lyo MOUSSU <moussu.lyo@gmail.com>
2025-08-13 16:41:53 +0200

    Web UI System

--- IStockIt/Program.cs
+++ IStockIt/Program.cs
@@ -1,25 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Permet les fichiers statiques depuis wwwroot/
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// Pour les fichiers statiques
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapRazorPages();
+
+// Exemple route simple
+app.MapGet("/hello", () => "Hello depuis ASP.NET Core !");
 
 app.Run();
--- IStockIt/obj/Debug/net8.0/IStockIt.AssemblyInfo.cs
+++ IStockIt/obj/Debug/net8.0/IStockIt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IStockIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2de8784e96a5249b8742b62e64fe6714580f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IStockIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IStockIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
